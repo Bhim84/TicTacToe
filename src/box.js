75c9e41912d92981b96022ad90856562a9d4c1ec
Bhim84 <37284961+Bhim84@users.noreply.github.com>
2018-06-05 13:06:33 +0530

    ...

--- src/box.js
+++ src/box.js
@@ -3,13 +3,43 @@ import React, { Component } from "react";
 export default class Box extends Component {
   constructor(props) {
     super(props);
-    this.style = {
-      height: this.props.size,
-      width: this.props.size
+    this.state = {
+      style: {
+        height: this.props.size,
+        width: this.props.size,
+        backgroundColor: "black"
+      },
+      player: ""
     };
+    this.select = this.select.bind(this);
+  }
+
+  select() {
+    if (this.state.player) {
+      return;
+    }
+    this.props.onClick(this.props.row, this.props.col);
+    this.setState(prevState => ({
+      style: {
+        ...prevState.style,
+        backgroundColor: "grey"
+      },
+      player: this.state.player === "" ? this.props.player : ""
+    }));
   }
 
   render() {
-    return <div style={this.style} id="box" className="box" />;
+    return (
+      <div
+        id={this.props.id}
+        row={this.props.row}
+        col={this.props.col}
+        style={this.state.style}
+        onClick={this.select}
+        className="box"
+      >
+        <span>{this.state.player}</span>
+      </div>
+    );
   }
 }
--- src/index.js
+++ src/index.js
@@ -7,10 +7,44 @@ class TicTacToe extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      board: [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
-      boxSize: 100
+      board: [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+      boxSize: 100,
+      player: 1
     };
     this.drawBox = this.drawBox.bind(this);
+    this.select = this.select.bind(this);
+  }
+
+  select(row, col) {
+    console.log("From index: ", row + ":" + col);
+    let updatedBoard = this.state.board;
+    updatedBoard[row][col] = this.state.player;
+    this.setState({
+      player: this.state.player === 2 ? 1 : 2,
+      board: updatedBoard
+    });
+  }
+
+  check() {
+    let b = this.state.board;
+    for (var i = 0; i < b.length; i++) {
+      if ((b[i][0] === b[i][1]) === b[i][2]) {
+        return true;
+      }
+    }
+    for (var i = 0; i < b.length; i++) {
+      if ((b[0][i] === b[1][i]) === b[2][i]) {
+        return true;
+      }
+    }
+    return (
+      (b[0][0] === b[1][1]) === b[2][2] ||
+      (b[1][0] === b[1][1]) === b[1][2] ||
+      (b[2][0] === b[2][1]) === b[2][2] ||
+      (b[0][0] === b[1][0]) === b[2][0] ||
+      (b[0][0] === b[0][1]) === b[0][2] ||
+      (b[0][0] === b[0][1]) === b[0][2]
+    );
   }
 
   componentWillMount() {
@@ -21,12 +55,19 @@ class TicTacToe extends React.Component {
     };
   }
 
-  drawBox(row, key) {
+  drawBox(row, rowId) {
     var output = (
-      <tr key={key}>
-        {row.map((box, index) => (
+      <tr key={rowId}>
+        {row.map((col, colId) => (
           <td>
-            <Box id={box} size={this.state.boxSize} />
+            <Box
+              row={rowId}
+              col={colId}
+              id={colId}
+              onClick={this.select}
+              player={this.state.player}
+              size={this.state.boxSize}
+            />
           </td>
         ))}
       </tr>
@@ -36,11 +77,14 @@ class TicTacToe extends React.Component {
 
   render() {
     return (
-      <table style={this.style}>
-        <tbody>
-          {this.state.board.map((row, key) => this.drawBox(row, key))}
-        </tbody>
-      </table>
+      <div style={this.style}>
+        <span id="playerDetails">Player {this.state.player} Turn</span>
+        <table>
+          <tbody>
+            {this.state.board.map((row, key) => this.drawBox(row, key))}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
